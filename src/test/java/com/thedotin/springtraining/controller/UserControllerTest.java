e0e147f2a934dfbea2f202b4b153c95bb6f10a09
Catalin Barcau <catalin.barcau@gmail.com>
2018-10-22 06:50:39 -0400

    Cleanup (no more argumentcaptors, imports, commented out code) and the remaining unit tests

--- src/test/java/com/thedotin/springtraining/controller/UserControllerTest.java
+++ src/test/java/com/thedotin/springtraining/controller/UserControllerTest.java
@@ -1,17 +1,15 @@
 package com.thedotin.springtraining.controller;
 
+import com.thedotin.springtraining.domain.User;
 import com.thedotin.springtraining.repository.UserRepository;
-import com.thedotin.springtraining.specifications.builders.UserSpecificationsBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 
 
@@ -21,92 +19,57 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({UserSpecificationsBuilder.class, UserRepository.class, UserController.class})
+@PrepareForTest({UserRepository.class, UserController.class})
 public class UserControllerTest {
 
     UserRepository mockRepo;
     Page mockPage;
-    UserSpecificationsBuilder mockUserBuilder;
+    User mockUser;
     UserController subject;
     Specification mockSpecification;
+    Pageable mockPageable;
 
     @Before
-    public void setUp() throws Exception {
-//        mockUserBuilder = PowerMockito.mock(UserSpecificationsBuilder.class);
-//        whenNew(UserSpecificationsBuilder.class).withAnyArguments().thenReturn(mockUserBuilder);
-        mockRepo = PowerMockito.mock(UserRepository.class);
-        mockPage = PowerMockito.mock(Page.class);
-        mockSpecification = PowerMockito.mock(Specification.class);
+    public void setUp() {
+        mockRepo = mock(UserRepository.class);
+        mockPage = mock(Page.class);
+        mockSpecification = mock(Specification.class);
+        mockPageable = mock(Pageable.class);
+        mockUser = mock(User.class);
         subject = new UserController(mockRepo);
+    }
+
+    @After
+    public void after() {
+        verifyNoMoreInteractions(mockRepo);
     }
 
     @Test
     public void findAllTest() throws Exception {
-
         doReturn(mockPage).when(mockRepo).findAll(any(Specification.class), any(Pageable.class));
-//        whenNew(UserSpecificationsBuilder.class).withAnyArguments().thenReturn(mockUserBuilder);
-//        doReturn(mockUserBuilder).when(mockUserBuilder).with(anyString(), anyString(), anyObject());
-//        when(mockUserBuilder.build()).thenReturn(mockSpecification);
         when(mockPage.getContent()).thenReturn(new ArrayList());
-
-        Pageable p = new MyPageable();
-        List results = subject.getUsers(p, "foo:bar");
-
-//        ArgumentCaptor<Specification> specificationCaptor = ArgumentCaptor.forClass(Specification.class);
-//        ArgumentCaptor<Pageable> pageableCaptor = ArgumentCaptor.forClass(Pageable.class);
-//        verify(mockRepo, times(1)).findAll(specificationCaptor.capture(), pageableCaptor.capture());
+        List results = subject.getUsers(mockPageable, "foo:bar");
         verify(mockRepo, times(1)).findAll(any(Specification.class), any(Pageable.class));
-//        assertThat(specificationCaptor.getValue(), is(mockSpecification));
-//        assertThat(pageableCaptor.getValue(), is(p));
-
         assertThat(results.size(), is(0));
-        verifyNoMoreInteractions(mockRepo);
-
     }
 
-    private static class MyPageable implements Pageable {
-        @Override
-        public int getPageNumber() {
-            return 0;
-        }
-
-        @Override
-        public int getPageSize() {
-            return 0;
-        }
-
-        @Override
-        public long getOffset() {
-            return 0;
-        }
-
-        @Override
-        public Sort getSort() {
-            return null;
-        }
-
-        @Override
-        public Pageable next() {
-            return null;
-        }
-
-        @Override
-        public Pageable previousOrFirst() {
-            return null;
-        }
-
-        @Override
-        public Pageable first() {
-            return null;
-        }
-
-        @Override
-        public boolean hasPrevious() {
-            return false;
-        }
+    @Test
+    public void createUserTest() {
+        doReturn(mockUser).when(mockRepo).save(any(User.class));
+        subject.createUser(mockUser);
+        verify(mockRepo, times(1)).save(any(User.class));
     }
+
+    @Test
+    public void deleteUserTest() {
+        doNothing().when(mockRepo).deleteById(any(Integer.class));
+        subject.deleteUser(100);
+        verify(mockRepo).deleteById(any(Integer.class));
+    }
+
 }
